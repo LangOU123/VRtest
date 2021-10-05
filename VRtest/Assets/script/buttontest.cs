85a1085dfe559036b0c3f41fd0fed5536d554af6
DESKTOP-HGOAV6G\lannnnnnngOY <1921364274@qq.com>
2021-10-05 10:34:33 +0800

    导入

---
+++ VRtest/Assets/script/buttontest.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class buttontest : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void dianji()
+    {
+        Debug.Log("点到了");
+    }
+}
